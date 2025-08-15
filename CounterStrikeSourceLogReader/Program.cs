8a780e39420a2e2ba1087cbe4102b90518831001
urantij <urantij@gmail.com>
2025-08-15 18:32:35 +0700

    Добавит сравниваемые группы

--- CounterStrikeSourceLogReader/Program.cs
+++ CounterStrikeSourceLogReader/Program.cs
@@ -1,16 +1,7 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
-
-namespace CounterStrikeSourceLogReader;
+﻿namespace CounterStrikeSourceLogReader;
 
 internal class Program
 {
-    private static string _path = "./log";
-    private static string _outPath = "./file";
-
-    private static Regex _regex = new("(?<target>.+)");
-
-    private static string _outFormat = "[target]";
     // Я надеюсь, ивенты в вочере синхронные, и мне не нужно ниче локать и думать
 
     private static TimeSpan AfkTimeout { get; } = TimeSpan.FromHours(2);
@@ -22,34 +13,36 @@ internal class Program
         if (args.Length != 4)
         {
             Console.WriteLine(
-                "нужны три аргумента: путь до логов; путь куда какать; регекс как парсить строки; аутпут формат строки;");
+                "нужны четыре аргумента: путь до логов; путь куда какать; регекс как парсить строки; аутпут формат строки;");
             Console.WriteLine("прес ентер ту екзит...");
             Console.ReadLine();
             return;
         }
 
-        _path = args[0]; // "/run/media/punky/Master/Dumbass/FastPunk/Steam/steamapps/common/Counter-Strike Source/cstrike/console.log";
-        _outPath = args[1]; // "./yes";
-        _regex = new Regex(args[2], RegexOptions.Compiled);
-        _outFormat = args[3];
+        // "/run/media/punky/Master/Dumbass/FastPunk/Steam/steamapps/common/Counter-Strike Source/cstrike/console.log";
+        string path = args[0];
+        // "./yes";
+        string outPath = args[1];
+        string regexFormat = args[2];
+        string outFormat = args[3];
 
         CancellationTokenSource cts = new();
 
-        Temptation temptation = new(_outFormat);
+        Temptation temptation = new(regexFormat, outFormat);
 
-        MyCoolWriter writer = new(_outPath);
+        MyCoolWriter writer = new(outPath);
         writer.Start();
 
         var killerInstinct = KillerInstinct.Create(AfkTimeout, cts);
 
         TrayBaker.Create(cts);
 
-        FileSystemWatcher watcher = new(Path.GetDirectoryName(_path) ?? throw new Exception("Путь какой то дебильный."),
-            Path.GetFileName(_path));
+        FileSystemWatcher watcher = new(Path.GetDirectoryName(path) ?? throw new Exception("Путь какой то дебильный."),
+            Path.GetFileName(path));
         // без файл нейма не видит что файл +тут или -тут
         watcher.NotifyFilter = NotifyFilters.Size | NotifyFilters.FileName;
 
-        MyCoolReader coolReader = new(_path, watcher, killerInstinct);
+        MyCoolReader coolReader = new(path, watcher, killerInstinct);
         coolReader.GotLine += line => CoolReaderOnGotLine(line, temptation, writer);
         coolReader.Start();
 
--- CounterStrikeSourceLogReader/Temptation.cs
+++ CounterStrikeSourceLogReader/Temptation.cs
@@ -2,18 +2,78 @@ using System.Text.RegularExpressions;
 
 namespace CounterStrikeSourceLogReader;
 
+enum TargetType
+{
+    Timespan
+}
+
+enum CompareType
+{
+    Less
+}
+
+class CompareInfo(TargetType targetType, CompareType compareType)
+{
+    public TargetType TargetType { get; } = targetType;
+    public CompareType CompareType { get; } = compareType;
+
+    public IComparable? CurrentValue { get; set; }
+}
+
+class GroupInfo(string name, CompareInfo? compare)
+{
+    public string Name { get; } = name;
+    public CompareInfo? Compare { get; } = compare;
+
+    public string? CurrentValue { get; set; }
+}
+
 /// <summary>
 /// Следит за темплейтом и позволяет сравнивать получаемую строку на необходимость написать её.
+/// Необходимость написать наступает, если строка совпадает с темплейтом, и хотя бы одна из её групп изменилась.
+/// Если в группе есть тип и сравнение, группа считается изменённой, если условие сравнения будет выполнено.
 /// </summary>
 public class Temptation
 {
-    private readonly string _template;
+    private readonly string _outputTemplate;
     private readonly Regex _regex;
 
-    public Temptation(string template)
+    private readonly GroupInfo[] _groups;
+
+    public Temptation(string regexString, string outputTemplate)
     {
-        _template = template;
-        _regex = new Regex(template, RegexOptions.Compiled);
+        _outputTemplate = outputTemplate;
+
+        // TODO можно было бы проверить, что все группы в темплейте присутствуют в регексе
+
+        MatchCollection matches = Regex.Matches(outputTemplate, @"\[(?<name>.+?)(\:(?<type>.+?)\:(?<compare>.+?))*\]");
+
+        _groups = matches.Select(match =>
+        {
+            CompareInfo? compare = null;
+
+            if (match.Groups["type"].Success)
+            {
+                TargetType targetType = Enum.Parse<TargetType>(match.Groups["type"].Value, true);
+                CompareType compareType = Enum.Parse<CompareType>(match.Groups["compare"].Value, true);
+
+                compare = new CompareInfo(targetType, compareType);
+            }
+
+            return new GroupInfo(match.Groups["name"].Value, compare);
+        }).ToArray();
+
+        // нужно сделать "чистый" темплейт без доп инфы
+        foreach (Match match in matches.OrderByDescending(m => m.Index))
+        {
+            if (!match.Groups["type"].Success)
+                continue;
+
+            _outputTemplate = _outputTemplate.Remove(match.Index, match.Length);
+            _outputTemplate = _outputTemplate.Insert(match.Index, $"[{match.Groups["name"]}]");
+        }
+
+        _regex = new Regex(regexString, RegexOptions.Compiled);
     }
 
     /// <summary>
@@ -26,7 +86,9 @@ public class Temptation
         if (!match.Success)
             return null;
 
-        string result = _template;
+        string result = _outputTemplate;
+
+        bool changed = false;
 
         foreach (Group matchGroup in match.Groups)
         {
@@ -34,9 +96,68 @@ public class Temptation
             if (string.IsNullOrEmpty(matchGroup.Name))
                 continue;
 
+            GroupInfo? groupInfo =
+                _groups.FirstOrDefault(g => g.Name.Equals(matchGroup.Name, StringComparison.OrdinalIgnoreCase));
+
+            // нонейм группы имеют нейм номерной.
+            if (groupInfo == null)
+                continue;
+
             result = result.Replace($"[{matchGroup.Name}]", matchGroup.Value);
+
+            if (groupInfo.CurrentValue != null)
+            {
+                if (groupInfo.CurrentValue == matchGroup.Value)
+                    continue;
+            }
+
+            groupInfo.CurrentValue = matchGroup.Value;
+
+            if (groupInfo.Compare != null)
+            {
+                IComparable newValue = MakeComparableObject(groupInfo.CurrentValue, groupInfo.Compare.TargetType);
+                IComparable? oldValue = groupInfo.Compare.CurrentValue;
+
+                groupInfo.Compare.CurrentValue = newValue;
+
+                if (oldValue != null)
+                {
+                    changed = Compare(oldValue, newValue, groupInfo.Compare.CompareType);
+                }
+                else
+                {
+                    changed = true;
+                }
+            }
+            else
+            {
+                changed = true;
+            }
         }
 
-        return result;
+        if (changed)
+            return result;
+
+        return null;
+    }
+
+    private static bool Compare(IComparable oldValue, IComparable newValue, CompareType compareType)
+    {
+        int sort = newValue.CompareTo(oldValue);
+
+        return compareType switch
+        {
+            CompareType.Less => sort < 0,
+            _ => throw new Exception($"Неизвестное сравнение {compareType}")
+        };
+    }
+
+    private static IComparable MakeComparableObject(string value, TargetType targetType)
+    {
+        return targetType switch
+        {
+            TargetType.Timespan => TimeSpan.Parse(value),
+            _ => throw new Exception($"Неизвестный тип типочек {targetType}")
+        };
     }
 }
